a27da11c54168012c786cda43927ede2af2401c7
AndreaPlm <palma.a.andrea@gmail.com>
2018-10-16 14:24:08 +0200

    update 14h30

--- lib/tictactoe.rb
+++ lib/tictactoe.rb
@@ -4,29 +4,45 @@ require 'rubocop'
 
 class Game # Une partie contient des joueurs et un plateau
 
-  @players = []
+=begin
+    def get_players()
+      @players = []
+
+      puts "Player 1: enter your name"
+      name1 =  gets.chomp
+      player1 = Players.new(name1)
+      @players << player1
+      puts "Player 2: enter your name"
+      name2 = gets.chomp
+      player2 = Players.new(name2)
+      @players << player2
+    end
+=end
+
 
 end # End Game
 
 
 class Player # Chaque joueur possède un nom et un score
-  attr_accessor :name, :score
+  attr_accessor :player_name, :score
 
   def initialize(name)
-    @name = name
+
+    @player_name = name
     @score = 0
   end
+
 end # End Player
 
 
 class Board # Un plateau contient des cases
 
-  def initialize # A la creation un plateau est vide
+  def initialize # A la creation un plateau est vide, on le rempli de cases vides
     @board_cases = []
   end
 
   # Remplir un plateau avec des cases
-  def fulfill_board(x=3, y=x)
+  def fill_board(x=3, y=x)
     x.times do |x|
       y.times do |y|
         board_case = BoardCase.new(x,y)
@@ -34,6 +50,13 @@ class Board # Un plateau contient des cases
       end
     end
   end
+
+  def board_display()
+    @board_cases.each do |elem|
+      elem.case_display
+    end
+  end
+
 
 end # End Board
 
@@ -46,8 +69,8 @@ class BoardCase # Les cases d'un plateau
   def initialize(abs, ord)
     @abs = abs
     @ord = ord
-    @name = "#{abs+1}#{convert_ord_to_letter(ord)}"
-    @value = 0
+    @case_name = "#{abs+1}#{convert_ord_to_letter(ord)}"
+    @val = 0
   end
 
   # Convertir les ordonnées en lettre afin de nommer correctement une case
@@ -58,14 +81,25 @@ class BoardCase # Les cases d'un plateau
 
   # Définir le statut de la case [X] [O] [ ]
   def case_to_x()
-    @value = 1 # [X] = 1
+    @val = 1 # [X] = 1
   end
   def case_to_o()
-    @value = 2 # [O] = 2
+    @val = 2 # [O] = 2
   end
   def case_to_empty()
-    @value = 0 # [ ] = 0
+    @val = 0 # [ ] = 0
   end
+
+ def case_display()
+   if @val == 0
+    print "| |"
+  elsif @val == 2
+    print "|O|"
+  elsif @val == 1
+    print "|X|"
+  end
+end
+
 
 end # End BoardCase
 
